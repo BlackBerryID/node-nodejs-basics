4bbb50960c8b4a0f2fed6ccdc0b016c2694ba795
BlackBerryID <iddf1994one@gmail.com>
2022-05-30 22:01:52 +0300

    feat: done fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import {writeFile} from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 const create = async () => {
-    // Write your code here 
+
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const pathName = path.join(__dirname, '/files/fresh.txt')
+
+    writeFile(pathName, 'I am fresh and young', {encoding: 'utf-8', flag: 'wx'}, (err) => {
+        if (err) {
+            if (err.code === 'EEXIST') {
+                throw new Error('FS operation failed')
+            }
+    }})
 };
 
-await create();
+await create()
