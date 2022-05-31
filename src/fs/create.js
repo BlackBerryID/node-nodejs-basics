67438b46a2ca5e3f760309a8f28f877d88f6a414
BlackBerryID <iddf1994one@gmail.com>
2022-05-31 22:02:28 +0300

    refactor: change syntax of fs module from callback to promise

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,19 +1,21 @@
-import {writeFile} from 'fs';
+import {writeFile} from 'fs/promises';
 import path from 'path';
 import {fileURLToPath} from 'url';
 
 const create = async () => {
-
-    const __filename = fileURLToPath(import.meta.url)
-    const __dirname = path.dirname(__filename)
-    const pathName = path.join(__dirname, '/files/fresh.txt')
-
-    writeFile(pathName, 'I am fresh and young', {encoding: 'utf-8', flag: 'wx'}, (err) => {
-        if (err) {
-            if (err.code === 'EEXIST') {
-                throw new Error('FS operation failed')
-            }
-    }})
+    try {
+        const __filename = fileURLToPath(import.meta.url)
+        const __dirname = path.dirname(__filename)
+        const pathName = path.join(__dirname, '/files/fresh.txt')
+    
+        await writeFile(pathName, 'I am fresh and young', {encoding: 'utf-8', flag: 'wx'})
+    } catch(err) {
+        if (err.code === 'EEXIST') {
+            throw new Error('FS operation failed')
+        } else {
+            throw err
+        }
+    }
 };
 
 await create()
