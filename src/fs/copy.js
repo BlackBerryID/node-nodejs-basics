1db9b4b18ac89114c465b1c8812d30ac84b2c096
BlackBerryID <iddf1994one@gmail.com>
2022-06-01 22:11:45 +0300

    feat: done fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+
+import {readdir, mkdir, copyFile} from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const __filename = fileURLToPath(import.meta.url)
+        const __dirname = path.dirname(__filename)
+        const folderPath = path.join(__dirname, 'files')
+        const targetFolderPath = `${folderPath}_copy`
+
+        const files = await readdir(folderPath)
+        await mkdir(targetFolderPath)
+        files.forEach(file => copyFile(`${folderPath}/${file}`, `${targetFolderPath}/${file}`))
+    } catch(err) {
+        if (err.code === 'EEXIST' || err.code === 'ENOENT') {
+            throw new Error('FS operation failed')
+        } else {
+            throw err
+        }
+    } 
 };
 
-await copy();
+await copy()
