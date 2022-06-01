db3c955e7a6c021f3c935230facf6fc79b52d546
BlackBerryID <iddf1994one@gmail.com>
2022-06-01 22:29:05 +0300

    feat: done fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+
+import {readFile} from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const __filename = fileURLToPath(import.meta.url)
+        const __dirname = path.dirname(__filename)
+        const folderPath = path.join(__dirname, 'files')
+
+        const file = await readFile(`${folderPath}/fileToRead.txt`, 'utf-8')
+        console.log(file)
+    } catch(err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed')
+        } else {
+            throw err
+        }
+    }  
 };
 
-await read();
+await read()
