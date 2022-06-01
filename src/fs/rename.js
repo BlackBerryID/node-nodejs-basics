77176e898bb2534a53e214b64c895d18c5bbd8b0
BlackBerryID <iddf1994one@gmail.com>
2022-06-01 22:18:53 +0300

    feat: done fs/rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import {rename as fspromiseRename} from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        const __filename = fileURLToPath(import.meta.url)
+        const __dirname = path.dirname(__filename)
+        const folderPath = path.join(__dirname, 'files')
+
+        await fspromiseRename(`${folderPath}/wrongFilename.txt`, `${folderPath}/properFilename.md`)
+    } catch(err) {
+        if (err.code === 'EEXIST' || err.code === 'ENOENT') {
+            throw new Error('FS operation failed')
+        } else {
+            throw err
+        }
+    }  
 };
 
-await rename();
+await rename()
