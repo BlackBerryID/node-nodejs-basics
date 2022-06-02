dae20ae200441eff89ae3a664d61aee71d50ebb6
BlackBerryID <iddf1994one@gmail.com>
2022-06-02 21:43:09 +0300

    feat: done cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+<<<<<<< HEAD
 const parseEnv = () => {
     // Write your code here 
 };
 
 parseEnv();
+=======
+export const parseEnv = () => {
+    let envArray = Object.entries(process.env)
+    const tempArr = []
+    envArray = envArray.filter(item => item[0].startsWith('RSS_'))
+    envArray.forEach(item => tempArr.push(item[0] + '=' + item[1]))
+    const result = tempArr.join('; ')
+    console.log(result)
+};
+
+parseEnv()
+>>>>>>> 4369e71 (feat: done cli/env.js)
