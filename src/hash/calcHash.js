10ba1b5079bfc9523a33cd33680eb705369eafcc
BlackBerryID <iddf1994one@gmail.com>
2022-06-02 22:37:40 +0300

    feat: done hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const file = await readFile('./files/fileToCalculateHashFor.txt')
+    const hashSum = createHash('sha256')
+    hashSum.update(file)
+    const hex = hashSum.digest('hex')
+    console.log(hex)
 };
 
-await calculateHash();
+await calculateHash()
