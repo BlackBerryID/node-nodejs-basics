e392d2f2b5eb11f3b84157ca0d36394e7ad9e1fa
BlackBerryID <iddf1994one@gmail.com>
2022-06-03 22:31:19 +0300

    feat: done streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+<<<<<<< HEAD
 const read = async () => {
     // Write your code here 
 };
 
 await read();
+=======
+import { stdout } from 'process';
+import { createReadStream } from 'fs';
+
+export const read = async () => {
+    const rs = createReadStream('./files/fileToRead.txt')
+    rs.pipe(stdout)
+};
+
+read()
+>>>>>>> f4d4289 (feat: done streams/read.js)
