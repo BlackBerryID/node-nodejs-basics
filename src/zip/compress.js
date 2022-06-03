3b30c468b0482cb86c68c20e2eddb67615eba865
BlackBerryID <iddf1994one@gmail.com>
2022-06-03 23:26:27 +0300

    feat: done zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,31 @@
+<<<<<<< HEAD
 const compress = async () => {
     // Write your code here 
 };
 
 await compress();
+=======
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+
+
+export const compress = async () => {
+    const gzip = createGzip()
+    const rs = createReadStream('./files/fileToCompress.txt')
+    const ws = createWriteStream('./files/archive.gz')
+
+    pipeline(
+        rs,
+        gzip,
+        ws,
+        err => {
+            if(err) {
+                console.log(err)
+            }
+        }
+    )
+};
+
+compress()
+>>>>>>> 97adf26 (feat: done zip/compress.js)
