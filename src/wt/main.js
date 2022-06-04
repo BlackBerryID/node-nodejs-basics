628ebe76ab8cc6e6745bc74d3171567370f7e7a4
BlackBerryID <iddf1994one@gmail.com>
2022-06-04 23:03:09 +0300

    feat: done wt/bothFiles

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpuCoresNumber = cpus().length
+    const resultArr = []
+    for (let i = 0; i < cpuCoresNumber; i++) {
+        resultArr.push(new Promise((resolve, reject) => {
+            const worker = new Worker('./worker.js', { workerData: i + 10})
+            worker.on('message', resolve)
+            worker.on('error', reject)
+        }))
+    }
+
+    const transformWorkerThreadResult = (result) => {
+        const copyObj = {...result}
+        if (result.status === 'fulfilled') {
+            copyObj.status = 'resolved'
+            copyObj.data = copyObj.value
+            delete copyObj.value
+        } else if (result.status === 'rejected') {
+            copyObj.status = 'error'
+            copyObj.data = null
+            delete copyObj.reason
+        }
+        return copyObj
+    }
+
+    Promise.allSettled(resultArr).then(results => console.log(results.map(result => transformWorkerThreadResult(result))))
 };
 
-await performCalculations();
+await performCalculations()
