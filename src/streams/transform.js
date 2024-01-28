12ad92d8bada86f74c4fadcaf4791784ef470533
Dmitry Khomichenko <dmitrii.khomichenko@pobeda.aero>
2024-01-28 13:52:53 +0300

    feat: solve streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { stdin, stdout } from 'process';
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({
+    transform(chunk, encoding, cb) {
+        cb(null, chunk.toString('utf-8').split('').reverse().join('') + '\n')
+    }
+})
+
+    stdin.pipe(transformStream).pipe(stdout)
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'fs';
+import { stdin } from 'process';
+
 const write = async () => {
-    // Write your code here 
-};
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const writeStream = createWriteStream(join(__dirname, 'files', 'fileToWrite.txt'))
+    stdin.pipe(writeStream)
+}
 
 await write();
