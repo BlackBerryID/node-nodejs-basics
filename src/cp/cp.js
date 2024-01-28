5ffd984dcc9232895470c83c010bfc20077c3d12
Dmitry Khomichenko <dmitrii.khomichenko@pobeda.aero>
2024-01-28 18:03:43 +0300

    feat: solve cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { fork } from 'child_process';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdin, stdout } from 'process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    
+    fork(join(__dirname, 'files', 'script.js'), args, {stdio: [stdin, stdout, 'ipc']})
 };
 
 // Put your arguments in function call to test this functionality
