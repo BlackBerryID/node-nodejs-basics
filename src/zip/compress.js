685c56503472f4301f32e5fa04b1107e4cadfee4
Dmitry Khomichenko <dmitrii.khomichenko@pobeda.aero>
2024-01-28 14:03:16 +0300

    feat: solve zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createGzip } from 'zlib';
+import { createWriteStream, createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const gzip = createGzip();
+    const readStream = createReadStream(join(__dirname, 'files', 'fileToCompress.txt'))
+    const writeStream = createWriteStream(join(__dirname, 'files', 'archive.gz'))
+
+    readStream.pipe(gzip).pipe(writeStream)
 };
 
 await compress();
